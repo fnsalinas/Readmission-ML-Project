071af53c354d06eb5a6d46a4562f45a4c4b0596b
Jose Ricardo Zapata <joser@pop-os.localdomain>
2020-09-12 19:12:58 -0500

    init repository with data science srtucture

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='analyze factors related to readmission as well as other outcomes pertaining to patients with diabetes.',
+    author='Jose R. Zapata',
+    license='MIT',
+)
